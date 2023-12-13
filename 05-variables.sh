7e58b94cf55c53b05850fd15d78b849b449959bd
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-13 13:11:01 +0530

    arguments

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#we can use arguments to dont disturb original code(args are $1,$2,$3.....)
+person1=$1
+person2=$2
+echo "$person1: Hello $person2, good morning"
+echo "$person2: Hi $person1, very good morning"
+echo "$person1: How are you Suresh?"
+echo "$person2: Iam good $person1 how are you?"
