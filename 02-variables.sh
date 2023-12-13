cf1c7bbae64395f55a44f034d8baec234dab7c55
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-13 07:10:14 +0530

    2 nd script about varibles

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Ramesh: Hello suresh, good morning"
+echo "Suresh: Hi Ramesh, very good morning"
+echo "Ramesh: How are you Suresh?"
+echo "Suresh: Iam good Ramesh how are you?"
+# In this script ramesh and suresh names are repeating,to overcome this we have variables in coding.
