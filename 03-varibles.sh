72c9bbc67a02ffbd19c268480bb396fc846fb4b0
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-13 12:34:14 +0530

    modidied

--- 03-varibles.sh
+++ 03-varibles.sh
@@ -2,8 +2,8 @@
 #we need to give variables means we store values in varaibles
 #but there is no space between variable and value.
 #And should we give "$" symbol before variable name in the code.
-person1=Ramesh
-person2=Suresh
+person1=Ram
+person2=Raheem
 echo "$person1: Hello $person2, good morning"
 echo "$person2: Hi $person1, very good morning"
 echo "$person1: How are you Suresh?"
