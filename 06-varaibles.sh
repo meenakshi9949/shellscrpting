78affaec5f0f0db51eeb6af3d28ba56052e2970e
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-13 13:43:31 +0530

    committed

--- 06-varaibles.sh
+++ 06-varaibles.sh
@@ -5,3 +5,7 @@ echo "username is: $USERNAME"
 #READ INPUT FROM THE USER THIS WILL SHOW IN TERMINAL 
 #read -s username
 #read -s password
+#username is visible in terminal so we have to give read -s command to hide
+echo "please enter your userame:" 
+read -s USERNAME #the value entered above is automatically attatched to USERNAME variable
+echo "username is: $USERNAME"
