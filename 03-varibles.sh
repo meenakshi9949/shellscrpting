353dbe365d94789a2891510c65e09057040592c2
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-13 12:27:57 +0530

    script related to variabe

---
+++ 03-varibles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#we need to give variables means we store values in varaibles
+#but there is no space between variable and value.
+#And should we give "$" symbol before variable name in the code.
+person1=Ramesh
+person2=Suresh
+echo "$person1: Hello $person2, good morning"
+echo "$person2: Hi $person1, very good morning"
+echo "$person1: How are you Suresh?"
+echo "$person2: Iam good $person1 how are you?"
