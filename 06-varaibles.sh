b9bb2899ad3b8997dd361dd9c05395006185807b
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-13 13:39:35 +0530

    committed

---
+++ 06-varaibles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter your userame:" 
+read USERNAME #the value entered above is automatically attatched to USERNAME variable
+echo "username is: $USERNAME"
+#READ INPUT FROM THE USER THIS WILL SHOW IN TERMINAL 
+#read -s username
+#read -s password
