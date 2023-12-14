d2a0b57f1e28174c4527e6aa1d0620e2dee94bff
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-14 17:22:44 +0530

    12-logs.sh

---
+++ 12-logs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ID=$(id -u)
+echo "script name :: $0"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+    echo "ERROR:: $2....failed"
+    exit 1 
+    else 
+    echo "$2.... success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+   echo "ERROR::please run this script with roo access" 
+exit 1 
+else 
+   echo "you are root user" 
+fi 
+yum install mysql -y 
+VALIDATE $? "installing mysql" #THIS IS FUNCTION
+yum install git -y 
+VALIDATE $? "installing git" #THIS IS FUNCTION
