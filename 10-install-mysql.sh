b9dce943ffefa400e92bb6d99c5d2bff6babbafe
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-14 16:12:05 +0530

    install mysql

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -8,7 +8,7 @@ else
 echo "you are root user"
 fi
 
-yum install mysqll -y
+yum install mysql -y
 if [ $? -ne 0 ]
 then 
    echo "installation of mysql is failed"
@@ -16,4 +16,12 @@ then
 else 
    echo "installation of mysql is success"
 fi
-   
+
+yum install git -y 
+if [ $? -ne 0 ]
+then 
+   echo "installation of mysql is failed"
+   exit 1
+else   
+   echo "installation of git is success"
+fi 
