acedd63d0b8a462bffc1df939ee23f5eaea1b450
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-14 18:42:38 +0530

    13-colours.sh

--- 13-colours.sh
+++ 13-colours.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
+
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
-echo ""script started executing $TIMESTAMP" &>> $LOGFILE
 
-echo "script name :: $0"
-VALIDATE(){ 
-if [ $1 -ne 0 ]
-then 
-    echo -e "ERROR:: $2....$R failed $N"
-    exit 1 
-else 
-    echo -e "$2.... $G success $N"
-fi
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "ERROR:: $2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
 if [ $ID -ne 0 ]
-then 
-   echo -e "$R ERROR::please run this script with roo access $N" 
-exit 1 
-else 
-   echo "you are root user" 
-fi 
-yum install mysqlL -y &>> $LOGFILE
-VALIDATE $? "installing mysql" #THIS IS FUNCTION
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $? "Installing MySQL"
+
 yum install git -y &>> $LOGFILE
-VALIDATE $? "installing git" #THIS IS FUNCTION
+
+VALIDATE $? "Installing GIT"
