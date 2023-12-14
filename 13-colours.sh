b5f9d37a80eb6a5d45e460a9796d40bc2e5b0b43
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-14 18:21:39 +0530

    13-colours.sh

---
+++ 13-colours.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo "script name :: $0"
+VALIDATE(){ 
+if [ $1 -ne 0 ]
+then 
+    echo -e "ERROR:: $2....$R failed $N"
+    exit 1 
+else 
+    echo -e "$2.... $G success"
+fi
+}
+
+if [ $ID -ne 0 ]
+then 
+   echo -e "$R ERROR::please run this script with roo access $N" 
+exit 1 
+else 
+   echo "you are root user" 
+fi 
+yum install mysql -y &>> $LOGFILE
+VALIDATE $? "installing mysql" #THIS IS FUNCTION
+yum install git -y &>> $LOGFILE
+VALIDATE $? "installing git" #THIS IS FUNCTION
