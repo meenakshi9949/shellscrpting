9d84b857cf8b278a25d0b9e5d929fad2c81c635e
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-14 13:00:53 +0530

    installing mysql

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -2,7 +2,8 @@
 ID=$(id -u)
 if [ $ID -ne 0 ]
 then
-echo "ERROT:: please run this script with root previleges"
+echo "ERROR:: please run this script with root previleges"
+exit 1 # you can give other than 0
 else
 echo "you are root user"
 fi
