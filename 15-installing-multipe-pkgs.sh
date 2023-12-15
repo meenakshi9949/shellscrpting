64fd4d3f3b637dfc336647182723e465f1b22174
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-15 20:28:41 +0530

    added

--- 15-installing-multipe-pkgs.sh
+++ 15-installing-multipe-pkgs.sh
@@ -9,13 +9,3 @@ else
 echo "YOU ARE ROOT USER" 
 fi    
 
-yum install mysql -y 
-# here we have to validate above command exuecution suuccess or failure
-
-if [ $? -ne 0 ]
-then 
-echo "ERROR:: INSTALLING MYSQL IS FAILURE" 
-exit 1 
-else 
-echo "INSTALLING MYSQL IS SUCCESS"
-fi
