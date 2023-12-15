5dfa80c586fb2d17bdf1a46925e48e0405fe00e1
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-15 20:30:48 +0530

    added

--- 15-installing-multipe-pkgs.sh
+++ 15-installing-multipe-pkgs.sh
@@ -3,9 +3,9 @@ ID=$(id -nu)
 
 if [ $ID -ne 0 ]
 then 
-echo "ERROR:: PLEASE RUN THIS CODE WITH ROOT ACCESS"
-exit 1 #here validate ID IS 0 OR non zero
+   echo "ERROR:: PLEASE RUN THIS CODE WITH ROOT ACCESS"
+
 else 
-echo "YOU ARE ROOT USER" 
+   echo "YOU ARE ROOT USER" 
 fi    
 
