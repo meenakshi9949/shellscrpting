ee2eea50a899251930b2cd3f9f7f1dd884cf46e1
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-15 20:38:38 +0530

    added

--- 15-installing-multipe-pkgs.sh
+++ 15-installing-multipe-pkgs.sh
@@ -2,9 +2,7 @@
 ID=$(id -u)
 if [ $ID -ne 0 ]
 then
-echo "ERROR:: please run this script with root previleges"
-
-else
-echo "you are root user"
+echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS" 
+else 
+echo "YOU ARE ROOT USER"
 fi
-
