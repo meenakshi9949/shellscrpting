0eee8e68809508ab57b760b9f41099e46bea66ca
meenakshi9949 <meenakshichiluveru@gmail.com>
2023-12-16 18:54:21 +0530

    added

--- 15-installing-multipe-pkgs.sh
+++ 15-installing-multipe-pkgs.sh
@@ -7,26 +7,26 @@ TIMESTAMP=$(date +%F-%H-%M-%S) #time
 LOGFILE="/tmp/$0-$TIMESTAMP.log" #this is log file
 
 R="\e[31m" #3colours
-G="\e[31m"
-Y="\e[32m"
+G="\e[32m"
+
 N="\e[0m" 
 
 VALIDATE(){ #function 
 if [ $1 -ne 0 ] 
 then 
-echo -e "error:: $2...  $R failed $N"  &>> $LOGFILE
+echo -e "error:: $2...  $R failed $N"  
 exit 1 #validate previous command ececute or not
 else 
-echo -e "$2...  $G success $N"  &>> $LOGFILE 
+echo -e "$2...  $G success $N"  
 fi        
 }
 
 if [ $ID -ne 0 ] #root or not 
 then
-echo -e "$R ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS $N"  &>> $LOGFILE
+echo -e "$R ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS $N" 
 exit 1
 else 
-echo -e "$G YOU ARE ROOT USER $N" &>> $LOGFILE
+echo -e "$G YOU ARE ROOT USER $N" 
 fi
 
 yum install mysql -y &>> $LOGFILE
